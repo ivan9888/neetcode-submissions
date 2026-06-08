0d8844062b462bef95b7bf81d3e4b8ebedaeb597
Ivan Andrade <92829634+ivan9888@users.noreply.github.com>
2026-06-08 17:49:48 -0600

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length()!=t.length()){return false;}
+        char[] x=s.toCharArray();
+        char[] y=t.toCharArray();
+
+        Arrays.sort(x);
+        Arrays.sort(y);
+        if(Arrays.equals(x,y)){return true;}
+        return false;
+    }
+}

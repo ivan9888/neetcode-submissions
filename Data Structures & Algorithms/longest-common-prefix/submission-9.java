1285aa21faa2365fb6a1567a24e7e4660644a915
Ivan Andrade <92829634+ivan9888@users.noreply.github.com>
2026-06-08 19:28:33 -0600

    Add: longest-common-prefix - submission-9

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-9.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        boolean out = false;
+        List<String> s=new ArrayList<>();
+        for(int x=0;x<strs[0].length() && !out ;x++)
+        {
+            int count=0;
+            for(int y=1;y<strs.length;y++)
+            {
+                if(x>=strs[y].length())
+                {
+                    out=true;
+                    break;
+                }
+                if(strs[0].charAt(x)==strs[y].charAt(x))
+                {count++;}
+            }
+            if(count==strs.length-1)
+            {
+                s.add(String.valueOf(strs[0].charAt(x)));
+            }
+            else
+            {
+                break;
+            }
+        }
+        if(s.size()>0)
+        {
+            return String.join("",s);
+        }
+        return "";
+
+    }
+}
